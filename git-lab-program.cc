37cfaeaa971bea6af0b400ee2d4afce1e3c628bc
Emma McAdams <em539719@ohio.edu>
2022-01-18 20:26:08 -0500

    git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+#git-lab-2
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Emma McAdams>
+ *        Date: <Tuesday, Jan 18 2022>
+ * Description: GitHub pushing and pulling
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+   cout << "Hey Bitch" << endl;
+   
+    return 0;
+}// main
